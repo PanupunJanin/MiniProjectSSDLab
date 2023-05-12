3a2d77feed1cc4354b49b3d03984cdda7b21645c
Panupun <panupan.ja@ku.th>
2023-05-12 21:44:50 +0700

    Implement basic AI

--- AI.java
+++ AI.java
@@ -1,3 +1,9 @@
 public class AI {
-
+    private boolean isOn;
+    private int difficulty;
+    public AI(int diff) {
+        isOn = false;
+        difficulty = diff;
+    }
+    public void startAI() { this.isOn = true; }
 }
