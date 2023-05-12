17a9b81e3971841b2568953510f7448847634a76
Tanabodee-Yambangyang <tanabodee.y@ku.th>
2023-05-12 22:26:58 +0700

    Unfinished moveable mark

--- Tile.java
+++ Tile.java
@@ -1,15 +1,20 @@
 public class Tile {
     private boolean hasPiece;
     private boolean isInPlay;
+    private boolean canMove;
     public Tile() {
         hasPiece = false;
         isInPlay = false; // In checker, the tiles that will be used is only black tiles.
+        canMove = false;
     }
     public boolean hasPiece() {
         return hasPiece;
     }
     public boolean isInPlay() {
         return isInPlay;
+    }
+    public boolean canMove() {
+        return canMove;
     }
     public void addPiece() {
         this.hasPiece = true;
@@ -19,5 +24,8 @@ public class Tile {
     }
     public void inPlay() {
         this.isInPlay = true;
+    }
+    public void setEnableMove(boolean set) {
+        this.canMove = set;
     }
 }
